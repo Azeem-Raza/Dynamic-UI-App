b31862a70d705fa589ec1935df6b7ef9d5bf3337
Azeem-Raza <azeemraza2001@gmail.com>
2024-10-22 19:05:56 +0300

    Created sketch for application

---
+++ app/src/main/java/com/azeem/dynamicuiapp/MainActivity.java
@@ -0,0 +1,40 @@
+package com.azeem.dynamicuiapp;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    //declare all feilds and buttons and layout
+
+    //initialize layout to vertical
+    //call createusernameinput function
+    //create createusernameinput function
+
+
+
+        //define feilds and hint add continue and back button
+        //add all fields to layout
+
+    //create createusernamecontinue function
+        //
+        // make username vriable to store
+        //field color to red
+
+
+    // create createpasswordinput for feilds and buttons
+
+
+    //create onpasswordcontinue function
+        //if field is empty turn red
+
+    //create showuserinfo
+        //variable for datetime and infotext
+}
